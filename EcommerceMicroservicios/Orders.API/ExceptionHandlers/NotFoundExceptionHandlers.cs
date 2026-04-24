cae8c75d3b39cf1d3097f9937e4f31cbc5a1b13e
Bautista Carpani <bautista.carpani@gmail.com>
2026-04-24 17:42:53 -0300

    Configuracion Orders 1 Exceptions ExceptionHandler HealthCheck

---
+++ EcommerceMicroservicios/Orders.API/ExceptionHandlers/NotFoundExceptionHandlers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Orders.API.Exceptions;
+
+namespace Orders.API.ExceptionHandlers;
+
+public class NotFoundExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext context,
+        Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is not NotFoundException ex)
+            return false;
+
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+
+        await context.Response.WriteAsJsonAsync(new ProblemDetails
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            Title = "Not Found",
+            Status = StatusCodes.Status404NotFound,
+            Detail = ex.Message,
+            Instance = context.Request.Path,
+            Extensions = { ["errorCode"] = ex.ErrorCode, ["errorMessage"] = ex.Message }
+        }, cancellationToken);
+
+        return true;
+    }
+}
