67e1685b015f601d0519adace15276c722402367
miradanielapaula-netizen <miradanielapaula@gmail.com>
2026-04-29 17:22:51 -0300

    errores products

---
+++ EcommerceMicroservicios/Products.API/Exceptions/ErrorCodes.cs
@@ -0,0 +1,10 @@
+namespace Products.API.Exceptions;
+
+public static class ErrorCodes
+{
+    public const string ProductoNoEncontrado      = "PRD-001";
+    public const string DatosInvalidos            = "PRD-002";
+    public const string NombreDuplicado           = "PRD-003";
+    public const string ProductoConOrdenesActivas = "PRD-004";
+    public const string ErrorInterno              = "PRD-005";
+}
